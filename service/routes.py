6f2a3b6e59796fcdb63969487b9aece0ca9b2942
Tony <tawn33y@gmail.com>
2024-03-06 18:35:45 +0000

    Remove dead code & increase coverage to 96.39%

--- service/routes.py
+++ service/routes.py
@@ -56,7 +56,6 @@ def index():
 ############################################################
 # Create product
 ############################################################
-PRODUCTS: dict = {}
 
 
 @app.route("/products", methods=["POST"])
@@ -68,10 +67,15 @@ def create_products():
     check_content_type("application/json")
 
     data = request.get_json()
-    id = data.get("id")
+    product_id = data.get("id")
 
-    if id in PRODUCTS:
-        error(status.HTTP_409_CONFLICT, f"Product with Id'{id}' already exists.")
+    if product_id:
+        product = Product.find(product_id)
+        if product:
+            error(
+                status.HTTP_409_CONFLICT,
+                f"Product with Id'{product_id}' already exists.",
+            )
 
     product = Product()
     product.deserialize(request.get_json())
